d26aa5841806c91789acedd128846666bb8d2f75
AshfakSakib <whateveritisuseless@gmail.com>
2025-11-22 00:59:41 +0100

    Which products contribute the most to profitability?

--- sql/01_top_5_products_by_margin.sql
+++ sql/01_top_5_products_by_margin.sql
@@ -1,1 +1,12 @@
--- Placeholder for query 1
+/*
+QUESTION: Which products contribute the most to profitability?
+*/
+
+SELECT 
+    p.product_name,
+    SUM(o.quantity * (p.unit_price - p.unit_cost)) AS total_margin
+FROM products p
+JOIN order_details o ON p.product_id = o.product_id
+GROUP BY p.product_name
+ORDER BY total_margin DESC
+LIMIT 5;-- Placeholder for query 1
