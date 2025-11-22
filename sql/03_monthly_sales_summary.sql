410b072f3d8719f67ecf6b08c2f58a990b48b439
AshfakSakib <whateveritisuseless@gmail.com>
2025-11-22 01:02:37 +0100

    How did total sales trend over the last three months?

--- sql/03_monthly_sales_summary.sql
+++ sql/03_monthly_sales_summary.sql
@@ -1,1 +1,11 @@
+/*
+QUESTION: How did total sales trend over the last three months?
+*/
 
+SELECT 
+    DATE_FORMAT(order_date, '%Y-%m') AS sales_month,
+    SUM(order_total) AS total_revenue
+FROM orders
+WHERE order_date >= DATE_SUB(CURRENT_DATE, INTERVAL 3 MONTH)
+GROUP BY 1
+ORDER BY 1 ASC;
