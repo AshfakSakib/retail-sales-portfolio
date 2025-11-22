2aa1f17be215af004372f02aa1512b729b8df90a
AshfakSakib <whateveritisuseless@gmail.com>
2025-11-22 01:03:26 +0100

    What is our current Average Order Value (AOV)?

--- sql/05_average_order_value.sql
+++ sql/05_average_order_value.sql
@@ -1,1 +1,8 @@
+/*
+QUESTION: What is our current Average Order Value (AOV)?
+*/
 
+SELECT 
+    AVG(order_total) AS average_order_value
+FROM orders
+WHERE order_date >= '2023-01-01'; -- Calculating for current year
